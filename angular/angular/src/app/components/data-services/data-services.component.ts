bfad092424154b115466d81aef5f3e44762782d8
Rudi Naparstek <rn0556728931@gmail.com>
2024-11-11 15:19:59 +0200

    start the angular project

---
+++ angular/angular/src/app/components/data-services/data-services.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-services',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-services.component.html',
+  styleUrl: './data-services.component.css'
+})
+export class DataServicesComponent {
+
+}
